f5d58bac58a1ac5fca31253985c647b36aec4f78
흐르미 <boyumi98@gmail.com>
2020-07-14 10:35:46 +0900

    MODIFY videoModel

--- app/models/video.js
+++ app/models/video.js
@@ -13,9 +13,17 @@ module.exports = (sequelize, DataTypes) => {
             type: DataTypes.STRING,
             allowNull: false,
         },
+        createdAt: {
+            allowNull: false,
+            type: DataTypes.DATE
+        },
+        updatedAt: {
+            allowNull: false,
+            type: DataTypes.DATE
+        }
     },
     { 
-        timestamps:true,
+        
     }
        
     )
